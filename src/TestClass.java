8fab71a10ae39854664ba89950a358da7f6a0469
Nur Trnova <88286456+nurtirnova@users.noreply.github.com>
2021-10-30 02:41:12 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,7 @@
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Hello from Git");
+        
+         System.out.println("Hello from Github");
     }
 }
